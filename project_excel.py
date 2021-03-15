94c8751dae05db0c37e63c6cc700d30be899ee9e
99003768g <undrajavarapu.vijay@ltts.com>
2021-03-15 16:45:27 +0530

    adding main poject duplicate with search functionality

--- project_excel.py
+++ project_excel.py
@@ -1,1 +1,24 @@
-print("replicate of main project")
+
+# Reading an excel file using Python
+import xlrd
+
+# Give the location of the file
+loc = "example.xls"
+
+# To open Workbook
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+word = "r2c2"
+#    input("enter the word to be found: ")
+print("you entered right????????")
+# For row 0 and column 0
+# for i in sheet:
+#    for j in sheet:
+if word == sheet.cell_value(2, 2):
+    print("found hurrahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+else:
+    print("not found")
+# print(sheet.cell_value(0, 1))
+# print(sheet.cell_value(2, 2))
+# print(sheet.cell_value(3, 3))
