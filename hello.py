f17c066f3522f638558e5705839f60b7861742ac
99003768g <78857841+99003768g@users.noreply.github.com>
2021-03-15 09:38:41 +0530

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,10 +1,3 @@
-print('hello python\n')
-a = 15
-b = 20
-print(a, b)
-print(type(a))
-print(id(a))
-a, b = b, a
-print(a, b)
-#changed new id
-print(id(a))
+name=input('enter your name: ')
+print("hello!! This is python code for hello {}".format(name))
+
