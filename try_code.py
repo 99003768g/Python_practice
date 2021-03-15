6a3868ab6224d64f67979ee0683a6e7789b9a732
99003768g <undrajavarapu.vijay@ltts.com>
2021-03-15 12:05:24 +0530

    updated try code

--- try_code.py
+++ try_code.py
@@ -1,1 +1,11 @@
 print("This is for try ")
+try:
+    a = 10
+    b = 0
+    c = a / b
+    print(c)
+
+except:
+    print("Can't divide with zero")
+finally:
+    print("This block is printed no matter what")
