2011000178c636cdf4e013e73387ee947b2d1d82
99003768g <78857841+99003768g@users.noreply.github.com>
2021-03-16 09:15:35 +0530

    update from linux

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
+print('hello python\n')
+a = 15
+b = 20
+print(a, b)
+print(type(a))
+print(id(a))
+a, b = b, a
+print(a, b)
+#changed new id
+print('a id is',id(a))
+print('b id is ',id(b))
 name=input('enter your name: ')
 print("hello!! This is python code for hello {}".format(name))
