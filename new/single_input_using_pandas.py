66342487412a216121fa84b7d0f32408edc7d6d7
99003768g <78857841+99003768g@users.noreply.github.com>
2021-03-20 11:50:15 +0530

    Add files via upload

---
+++ new/single_input_using_pandas.py
@@ -0,0 +1,91 @@
+import pandas as pd
+from matplotlib import pyplot as pt
+
+excel_file = pd.ExcelFile('book.xlsx')
+# PS = 0
+# NAME = ''
+# EMAIL = ''
+something = input("ENTER SOMETHING: ")
+
+if something == "PS":
+    PS = input("enter the PS: ")
+    df = pd.DataFrame()
+
+    for i in excel_file.sheet_names:
+        df1 = pd.read_excel(excel_file, i)
+        # check for input
+        df1.set_index('PS_Number', inplace=True)
+
+        result = df1.loc[input]
+        print(result)
+
+elif something == "NAME":
+    NAME = input("enter NAME: ")
+    df = pd.DataFrame()
+
+    for i in excel_file.sheet_names:
+        df1 = pd.read_excel(excel_file, i)
+
+        # check for input
+        df1.set_index('Name', inplace=True)
+
+        result = df1.loc[input]
+        print(result)
+
+
+elif something == "EMAIL":
+    EMAIL = input("enter Email: ")
+    df = pd.DataFrame()
+
+    for i in excel_file.sheet_names:
+        df1 = pd.read_excel(excel_file, i)
+
+else:
+    print("Not found re babaa")
+#
+#         # check for input
+#         df1.set_index('Email_Address', inplace=True)
+#
+#         result = df1.loc[input]
+#         print(result)
+# PS = 99003768
+# NAME = "Undrajavarapu Vijay"
+# EMAIL = "undrajavarapu.vijay@ltts.com"
+
+# if "something " == PS:
+#     df = pd.DataFrame()
+#
+#     for i in excel_file.sheet_names:
+#         df1 = pd.read_excel(excel_file, i)
+#         # check for input
+#         df1.set_index('PS_Number', inplace=True)
+#
+#         result = df1.loc[input]
+#         print(result)
+#
+# elif "something " == NAME:
+#     df = pd.DataFrame()
+#
+#     for i in excel_file.sheet_names:
+#         df1 = pd.read_excel(excel_file, i)
+#
+#         # check for input
+#         df1.set_index('Name', inplace=True)
+#
+#         result = df1.loc[input]
+#         print(result)
+#
+# elif "something " == EMAIL:
+#     df = pd.DataFrame()
+#
+#     for i in excel_file.sheet_names:
+#         df1 = pd.read_excel(excel_file, i)
+#
+#         # check for input
+#         df1.set_index('Email_Address', inplace=True)
+#
+#         result = df1.loc[input]
+#         print(result)
+# df = pd.DataFrame({'PS_Number': ['A', 'B', 'C'], 'Team_No_1': [10, 30, 20]})
+# ax = df.plot.bar(x='PS_Number', y='Team_No_1', rot=0)
+# pt.show()
